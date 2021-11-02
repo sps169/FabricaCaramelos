d9144322720f6f10c3ab19491cbafb985dfe54a7
Sergio <sps169@outlook.es>
2021-11-02 01:14:44 +0100

    DOM library implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,30 @@
+import model.Color;
+import model.Departamento;
+import model.Gnomo;
 import org.xml.sax.SAXException;
+import xmlControllers.DOM.DOMController;
 import xmlControllers.SAX.SAXController;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Christmas!");
-        SAXController sax =  SAXController.getInstance("data/FabricaCaramelos.xml");
+        String uri = "data/FabricaCaramelos.xml";
+        String uriMod = "data/FabricaCaramelosMod.xml";
+        //saxReading(uri);
+        //domReading(uri);
+        domModifying(uriMod);
+        domReading(uriMod);
+    }
+
+    public static void saxReading(String uri) {
+        SAXController sax =  SAXController.getInstance(uri);
         try {
             sax.loadData();
         } catch (ParserConfigurationException e) {
@@ -18,6 +35,46 @@ public class Main {
             e.printStackTrace();
         }
         sax.getDepartamentos().forEach(System.out::println);
+    }
 
+    public static void domReading(String uri) {
+        DOMController dom = DOMController.getInstance(uri);
+        try {
+            dom.loadData();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+        dom.getDepartamentos().forEach(System.out::println);
+    }
+
+    private static void domModifying(String uriMod) {
+        List<Gnomo> gnomos = new ArrayList<>();
+        gnomos.add(new Gnomo("g17", "Faker", Color.azul, 167));
+        gnomos.add(new Gnomo("g18", "Choumaker", Color.blanco, 168));
+        gnomos.add(new Gnomo("g19", "Pocho", Color.negro, 10));
+        Departamento departamento = new Departamento("5", "Investigar caramelos", gnomos);
+        DOMController dom = DOMController.getInstance(uriMod);
+        try {
+            dom.loadData();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+        dom.insertarDepartamento(departamento);
+        Gnomo gnomo = new Gnomo("g20", "Ezekiel", Color.blanco,101);
+        dom.insertarGnomo(gnomo, "5");
+        dom.updateGnomo("g20");
+        try {
+            dom.writeXMLFile(uriMod);
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/xmlControllers/DOM/DOMController.java
+++ src/main/java/xmlControllers/DOM/DOMController.java
@@ -1,14 +1,24 @@
 package xmlControllers.DOM;
 
+import model.Color;
+import model.Departamento;
+import model.Gnomo;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DOMController {
     private static DOMController instance;
@@ -43,5 +53,127 @@ public class DOMController {
         this.data.getDocumentElement().normalize();
     }
 
+    public List<Departamento> getDepartamentos() {
+        List<Departamento> departamentos = new ArrayList<>();
+        NodeList nodes = data.getElementsByTagName("departamento");
+        for (int i = 0; i < nodes.getLength(); i++) {
+            departamentos.add(getDepartamento(nodes.item(i)));
+        }
+        return departamentos;
+    }
 
+    private Departamento getDepartamento(Node node) {
+        if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("departamento")) {
+            Element element = (Element) node;
+            return new Departamento(element.getAttribute("id"),
+                    getTagValue("nombre_departamento", element),
+                    getGnomos(element.getElementsByTagName("gnomos").item(0)));
+        }
+        else return null;
+    }
+
+    private String getTagValue (String tag, Element element) {
+        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+        Node node = nodeList.item(0);
+        if(node != null)
+            return node.getNodeValue();
+        else return null;
+    }
+
+    private List<Gnomo> getGnomos (Node node) {
+        List<Gnomo> gnomos = new ArrayList<>();
+        if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("gnomos")) {
+            NodeList nodosGnomo = ((Element) node).getElementsByTagName("gnomo");
+            for (int i = 0; i < nodosGnomo.getLength(); i++) {
+                gnomos.add(getGnomo(nodosGnomo.item(i)));
+            }
+        }
+        return gnomos;
+    }
+
+    private Gnomo getGnomo (Node node) {
+        if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("gnomo")) {
+            Element element = (Element) node;
+            return new Gnomo(element.getAttribute("id"),
+                    getTagValue("nombre", element),
+                    Color.valueOf(getTagValue("color", element)),
+                    Integer.parseInt(getTagValue("iq", element)));
+        }
+        else return null;
+    }
+
+    private Node createElement (String tag, String value) {
+        Element node = data.createElement(tag);
+        node.appendChild(data.createTextNode(value));
+        return node;
+    }
+
+    private Node createElementGnomos (List<Gnomo> gnomos) {
+        Element node = data.createElement("gnomos");
+        gnomos.stream().forEach(s -> {
+            Node gnomeNode = createElementGnomo(s);
+            node.appendChild(gnomeNode);
+        });
+        return node;
+    }
+    public void insertarDepartamento(Departamento departamento) {
+        Element departamentoElement = this.data.createElement("departamento");
+        departamentoElement.setAttribute("id", departamento.getId());
+        departamentoElement.appendChild(createElement("nombre_departamento", departamento.getNombreDepartamento()));
+        departamentoElement.appendChild(createElementGnomos(departamento.getGnomos()));
+        this.data.getDocumentElement().appendChild(departamentoElement);
+    }
+
+    public void updateGnomo(String idGnomo) {
+        NodeList list = this.data.getElementsByTagName("gnomo");
+        for (int i = 0; i < list.getLength(); i++) {
+            if (list.item(i).getAttributes().getNamedItem("id").getNodeValue().equals(idGnomo)) {
+                ((Element)list.item(i)).getElementsByTagName("iq").item(0).getFirstChild().setNodeValue("0");
+            }
+        }
+    }
+
+    public void deleteElement(String idElement, String tag) {
+        NodeList list = this.data.getElementsByTagName(tag);
+        for (int i = 0; i < list.getLength(); i++) {
+            if (list.item(i).getAttributes().getNamedItem("id").getNodeValue().equals(idElement)) {
+                list.item(i).getParentNode().removeChild(list.item(i));
+            }
+        }
+    }
+
+    public void insertarGnomo(Gnomo gnomo, String idDepartamento) {
+        NodeList list = this.data.getElementsByTagName("departamento");
+        for (int i = 0; i < list.getLength(); i++) {
+            if (list.item(i).getNodeType() == Node.ELEMENT_NODE){
+                if (((Element) list.item(i)).getAttribute("id").equals(idDepartamento)) {
+                    Node gnomeNode = createElementGnomo(gnomo);
+                    ((Element) list.item(i)).getElementsByTagName("gnomos").item(0).appendChild(gnomeNode);
+                }
+            }
+        }
+    }
+
+    private Node createElementGnomo(Gnomo gnomo) {
+        Element gnomeNode = data.createElement("gnomo");
+        gnomeNode.setAttribute("id", gnomo.getId());
+        gnomeNode.appendChild(createElement("nombre", gnomo.getNombre()));
+        gnomeNode.appendChild(createElement("color", gnomo.getColor().name()));
+        gnomeNode.appendChild(createElement("iq", gnomo.getIq() + ""));
+        return gnomeNode;
+    }
+
+    private Transformer getPreprocessor() throws TransformerConfigurationException {
+        TransformerFactory trFactory = TransformerFactory.newInstance();
+        Transformer tr = trFactory.newTransformer();
+        tr.setOutputProperty(OutputKeys.INDENT, "yes");
+        return tr;
+    }
+
+    public void writeXMLFile(String uri) throws TransformerException {
+        Transformer transformer = getPreprocessor();
+        DOMSource source = new DOMSource(this.data);
+        StreamResult file = new StreamResult(new File(uri));
+        transformer.transform(source, file);
+    }
 }
